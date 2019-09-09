15a91d8d474a94319fdbfbf6b468af6c1fdfd0f2
ckonig <itckoenig@gmail.com>
2019-09-09 23:11:37 +0200

    basic UI with ability to send emails

--- mailaway/resources/js/components/SendMailForm.vue
+++ mailaway/resources/js/components/SendMailForm.vue
@@ -4,35 +4,37 @@
     <form>
       <div>
         <label for="senderName">Sender Name</label>
-        <input id="senderName" type="text" />
+        <input id="senderName" v-model="senderName" type="text" />
       </div>
 
       <div>
         <label for="senderEmail">Sender Email</label>
-        <input id="senderEmail" type="text" />
+        <input id="senderEmail" v-model="senderEmail" type="text" />
       </div>
 
       <div>
         <label for="title">Title</label>
-        <input id="title" type="text" />
+        <input id="title" v-model="title" type="text" />
       </div>
 
       <div>
         <label for="to">Recipients</label>
-        <input id="to" type="text" />
+        <input id="to" v-model="to" type="text" />
       </div>
 
       <div>
         <label for="body_txt">Content (text)</label>
-        <input id="body_txt" type="text" />
+        <input id="body_txt" v-model="body_txt" type="text" />
       </div>
 
       <div>
         <label for="body_html">Content (html)</label>
-        <input id="body_html" type="text" />
+        <input id="body_html" v-model="body_html" type="text" />
       </div>
 
       <button v-on:click="sendMail" type="button">Send Mail</button>
+
+      <span>{{result}}</span>
     </form>
   </div>
 </template>
@@ -40,9 +42,31 @@
 <script>
 import MailService from "../services/api/MailService";
 export default {
+  data: function() {
+    return {
+      title: "",
+      senderName: "",
+      senderEmail: "",
+      to: "",
+      body_txt: "",
+      body_html: "",
+      result: ""
+    };
+  },
   methods: {
     sendMail: function() {
-      MailService.sendMail();
+      MailService.sendMail(
+        this.title,
+        this.senderName,
+        this.senderEmail,
+        this.to,
+        this.body_txt,
+        this.body_html
+      ).then(mail => {
+        if (mail && mail.status && mail.id) {
+          this.result = mail.id + " - " + mail.status;
+        }
+      });
     }
   }
 };
--- mailaway/resources/js/services/api/MailService.js
+++ mailaway/resources/js/services/api/MailService.js
@@ -1,10 +1,21 @@
 import axios from 'axios';
 
 export default {
-    sendMail () {
-        console.log('todo implement');
+    sendMail(title, senderName, senderEmail, to, body_txt, body_html) {
+        return axios.post('/api/mail', {
+            title,
+            body_html,
+            body_txt,
+            to: to.split(',').map(item => item.trim()),
+            from: {
+                email: senderEmail,
+                name: senderName
+            }
+        }).then(response => {
+            return response.data;
+        });
     },
-    getMail (id) {
+    getMail(id) {
         return axios.get('/api/mail/' + id).then(response => {
             return response.data;
         });
